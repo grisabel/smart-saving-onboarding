62c670cd88e217e2dffe7c859c1ba519858749c3
Delgado Trueba <delgadotrueba@outlook.com>
2024-02-11 19:41:22 +0000

    feat/CA-2.2: create SigupContainer

--- src/components/pages/login/components/LoginLayoutDesktop.tsx
+++ src/components/pages/login/components/LoginLayoutDesktop.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./LoginLayoutDesktop.module.scss";
 
 import LoginContainer from "./LoginContainer";
+import SignupContainer from "./SignupContainer";
 
 const LoginLayoutDesktop: React.FC = () => {
   return (
     <div className={styles.loginLayout}>
       <LoginContainer />
+      <SignupContainer />
     </div>
   );
 };
