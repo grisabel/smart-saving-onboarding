574e8036d2b85000c169235148241428f018b199
IsabelGR <grisabel97@outlook.com>
2024-02-17 19:22:38 +0100

    feat/CA-3.1: retrievePasswordLayoutDesktop component

---
+++ src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutDesktop/RetrievePasswordLayoutDesktop.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./RetrievePasswordLayoutDesktop.module.scss";
+
+const RetrievePasswordLayoutDesktop: React.FC = () => {
+  return (
+    <div className={styles.retrievePassword}>
+      <div className={styles.logoStepts}>hola</div>
+      <div className={styles.form}>como</div>
+      <div className={styles.image}>estas</div>
+    </div>
+  );
+};
+
+export default RetrievePasswordLayoutDesktop;
