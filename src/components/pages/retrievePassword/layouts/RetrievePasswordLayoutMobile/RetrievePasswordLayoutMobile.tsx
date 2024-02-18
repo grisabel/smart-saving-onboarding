372950e91b380065aa9f57907e948b24531e0c1e
Delgado Trueba <jose.delgado@babel.es>
2024-02-18 19:01:07 +0100

    feat/CA-3.1:add  layoutMobile

--- src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutMobile/RetrievePasswordLayoutMobile.tsx
+++ src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutMobile/RetrievePasswordLayoutMobile.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-import styles from "./RetrievePasswordLayoutMobile.module.scss";
-import Logo from "@/components/stories/atoms/Logo";
 import RetrievePasswordForm from "../../components/retrievePasswordContainer/components/retrievePasswordForm";
 import DotStep from "@/components/stories/atoms/Steps/DotStep";
+import LayoutMobile from "@/components/stories/templates/LayoutMobile/LayoutMobile";
+
+import styles from "./RetrievePasswordLayoutMobile.module.scss";
 
 const RetrievePasswordLayoutMobile: React.FC = () => {
   return (
-    <div className={styles.retrievePasswordMobile}>
-      <Logo />
-      <RetrievePasswordForm />
-      <DotStep
-        steps={[
-          { isTransited: true },
-          { isTransited: false },
-          { isTransited: false },
-        ]}
-      ></DotStep>
-    </div>
+    <LayoutMobile>
+      <div className={styles.retrievePasswordMobile}>
+        <RetrievePasswordForm />
+        <DotStep
+          steps={[
+            { isTransited: true },
+            { isTransited: false },
+            { isTransited: false },
+          ]}
+        ></DotStep>
+      </div>
+    </LayoutMobile>
   );
 };
 
