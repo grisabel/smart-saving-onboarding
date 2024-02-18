1d9d497417221fa0b3f8951892a9b3826aafd2e2
Delgado Trueba <jose.delgado@babel.es>
2024-02-18 22:14:40 +0100

    feat/CA-3.1: img

--- src/components/pages/login/components/SignupContainer/SignupContainer.tsx
+++ src/components/pages/login/components/SignupContainer/SignupContainer.tsx
@@ -6,13 +6,15 @@ import Button from "@/components/stories/atoms/Buttons/Button";
 import styles from "./SignupContainer.module.scss";
 
 const SignupContainer: React.FC = () => {
+  const random = Math.floor(Math.random() * 4) + 1;
+
   return (
     <div className={styles.container}>
       <Image
-        src="/images/login/login_1.svg"
+        src={`/images/login/login_${random}.svg`}
         alt="login"
         width={100}
-        height={100}
+        height={357}
       ></Image>
       <p>{t("achieve-goals")}</p>
       <p>{t("create-account")}✨</p>
