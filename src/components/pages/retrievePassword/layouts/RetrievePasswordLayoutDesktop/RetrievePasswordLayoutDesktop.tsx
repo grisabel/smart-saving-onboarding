584bc8c8d0e0cd57f6f11f28bb52a55d882f95f0
IsabelGR <grisabel97@outlook.com>
2024-02-18 11:25:18 +0100

    feat/CA-3.1: retrievePasswordLayoutDesktop component

--- src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutDesktop/RetrievePasswordLayoutDesktop.tsx
+++ src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutDesktop/RetrievePasswordLayoutDesktop.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Image from "next/image";
+
 import styles from "./RetrievePasswordLayoutDesktop.module.scss";
+import Logo from "@/components/stories/Logo";
+import RetrievePasswordContainer from "../../components/retrievePasswordContainer/retrievePasswordContainer";
 
 const RetrievePasswordLayoutDesktop: React.FC = () => {
   return (
-    <div className={styles.retrievePassword}>
-      <div className={styles.logoStepts}>hola</div>
-      <div className={styles.form}>como</div>
-      <div className={styles.image}>estas</div>
+    <div className={styles.container}>
+      <Logo />
+      <div className={styles.retrievePassword}>
+        <div className={styles.stepts}></div>
+        <RetrievePasswordContainer />
+        <div className={styles.image}>
+          <Image
+            src="/images/retrievePassword/retrievePassword_1.svg"
+            alt="retrievePassword"
+            layout="fill"
+          ></Image>
+        </div>
+      </div>
     </div>
   );
 };
