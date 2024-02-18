9af9200ac11711c248d4ca2d882b17ee81903aec
Delgado Trueba <jose.delgado@babel.es>
2024-02-18 15:27:07 +0100

    feat/CA-3.1: add formater

--- src/components/stories/Inputs/InputBase/InputBase.tsx
+++ src/components/stories/Inputs/InputBase/InputBase.tsx
@@ -7,13 +7,34 @@ const InputBase: React.FC<InputBaseProps> = ({
   placeholder,
   type,
   icon,
+  value: defaultValue,
+  onChange,
+  formatValue,
   className,
 }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const formatedVal = formatValue ? formatValue(value) : value;
+
+    if (typeof onChange === "function") {
+      event.target.value = formatedVal;
+      event.currentTarget.value = formatedVal;
+
+      onChange(event);
+    }
+  };
+
   return (
     <div className={`${styles.inputWp} ${className}`}>
       <label className={styles.label}>{label}</label>
       <div className={styles.inputContainer}>
-        <input className={styles.input} placeholder={placeholder} type={type} />
+        <input
+          className={styles.input}
+          placeholder={placeholder}
+          type={type}
+          defaultValue={defaultValue ?? ""}
+          onChange={handleChange}
+        />
         {icon && icon}
       </div>
     </div>
--- src/components/stories/Inputs/InputBase/InputBase.types.tsx
+++ src/components/stories/Inputs/InputBase/InputBase.types.tsx
@@ -5,4 +5,8 @@ export interface InputBaseProps {
   placeholder?: string;
   type: "email" | "password" | "text" | "number";
   icon?: JSX.Element;
+
+  value?: string | null;
+  onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void;
+  formatValue?: (value: string) => string;
 }
