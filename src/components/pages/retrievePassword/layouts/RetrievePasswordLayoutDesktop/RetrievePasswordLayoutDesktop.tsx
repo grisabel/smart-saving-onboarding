398a7408b7e8f46c4ff53ee6bc4d03317457fc03
IsabelGR <grisabel97@outlook.com>
2024-02-18 18:20:10 +0100

    feat/CA-3.1: change styles layout

--- src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutDesktop/RetrievePasswordLayoutDesktop.tsx
+++ src/components/pages/retrievePassword/layouts/RetrievePasswordLayoutDesktop/RetrievePasswordLayoutDesktop.tsx
@@ -1,36 +1,29 @@
 import React from "react";
-import Image from "next/image";
 
-import styles from "./RetrievePasswordLayoutDesktop.module.scss";
-import Logo from "@/components/stories/atoms/Logo";
 import RetrievePasswordContainer from "../../components/retrievePasswordContainer/retrievePasswordContainer";
+import LayoutStepDesktop from "@/components/stories/templates/LayoutStepDesktop";
 import LineStep from "@/components/stories/atoms/Steps/LineStep";
 import Icon from "@/components/stories/atoms/Icon";
+import styles from "./RetrievePasswordLayoutDesktop.module.scss";
 
 const RetrievePasswordLayoutDesktop: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <Logo />
-      <div className={styles.retrievePassword}>
-        <div className={styles.stepts}>
-          <LineStep
-            steps={[
-              { icon: <Icon name="question" />, isCompleted: true },
-              { isTransited: false, icon: <Icon name="email-send" /> },
-              { isTransited: false, icon: <Icon name="lock" /> },
-            ]}
-          ></LineStep>
-        </div>
-        <RetrievePasswordContainer />
-        <div className={styles.image}>
-          <Image
-            src="/images/retrievePassword/retrievePassword_1.svg"
-            alt="retrievePassword"
-            layout="fill"
-          ></Image>
-        </div>
-      </div>
-    </div>
+    <LayoutStepDesktop
+      image={{
+        src: "/images/retrievePassword/retrievePassword_1.svg",
+        alt: "retrievePassword",
+      }}
+      step={
+        <LineStep
+          steps={[
+            { icon: <Icon name="question" />, isCompleted: true },
+            { isTransited: false, icon: <Icon name="email-send" /> },
+            { isTransited: false, icon: <Icon name="lock" /> },
+          ]}
+        ></LineStep>
+      }
+      form={<RetrievePasswordContainer />}
+    />
   );
 };
 
