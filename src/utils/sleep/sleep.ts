56503d2ecee3cd2af53111372360e3efba3af35b
Delgado Trueba <jose.delgado@babel.es>
2024-02-19 19:25:15 +0100

    feat/CA-3.1: pr-review

--- src/utils/sleep/sleep.ts
+++ src/utils/sleep/sleep.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-promise-executor-return */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-export function sleep(ms: number) {
+export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
