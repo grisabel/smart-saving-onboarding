b3c9c1f8fdf4f7f43fc281c059d92edf9ecf215b
Delgado Trueba <jose.delgado@babel.es>
2024-02-24 18:42:02 +0100

    feat/CA-2.3: add event form

--- src/components/pages/login/components/FormLogin/FormLogin.tsx
+++ src/components/pages/login/components/FormLogin/FormLogin.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
 
@@ -14,26 +14,42 @@ const sessionRepository = SessionFactoryRepository.getInstance();
 const FormLogin: React.FC = () => {
   const { t } = useTranslation();
 
-  React.useEffect(() => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
     sessionRepository
-      .login({ email: "a", password: "b" })
-      .then((resul) => console.log(resul));
-  }, []);
+      .login({ email: email, password: password })
+      .then((resul) => console.log(resul))
+      .catch((error) => console.log(error));
+  };
+
+  const handleInputEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleInputPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div className={styles.container}>
       <p className={styles.title}>{t("login now")}</p>
       <p className={styles.subtitle}>{t("better financial control")}</p>
-      <form className={styles.emailForm}>
+      <form className={styles.emailForm} onSubmit={handleSubmit}>
         <div className={styles.inputs}>
           <InputTextEmail
             className={styles.emailInput}
             label={t("input-email-label")}
             placeholder={t("input-email-placeholder")}
+            onChange={handleInputEmail}
           />
           <InputTextPassword
             label={t("input-password-label")}
             placeholder={t("input-password-placeholder")}
+            onChange={handleInputPassword}
           />
           <Link href="/retrieve-password" className={styles.forgotPassword}>
             {t("forgotPassword")}
