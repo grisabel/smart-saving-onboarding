15cf12ada6c7a07c58e869fd2ee03ca87f31e7d1
Delgado Trueba <jose.delgado@babel.es>
2024-02-25 19:53:09 +0100

    feat/CA-1.4: create model

---
+++ src/repository/UserRepository/model/request/RegisterRequestModel.ts
@@ -0,0 +1,10 @@
+export interface RegisterRequestModel {
+  firstName: string;
+  lastName: string;
+  dateBirth: string;
+  objetive: string;
+  email: string;
+  repeatEmail: string;
+  password: string;
+  repeatPassword: string;
+}
