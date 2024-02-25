5b468fb9d9d27e073cae91b1fa64e234dc1cc8c0
IsabelGR <grisabel97@outlook.com>
2024-02-25 16:30:46 +0100

    feat/CA-3.1: reset error desktop

---
+++ src/pages/retrieve-password/reset/error/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+import MainLayout from "@/components/stories/templates/LayoutMain/LayoutMain";
+import ResetPasswordErrorDesktop from "@/components/pages/resetPasswordError/layouts/desktop";
+import ResetPasswordErrorMobile from "@/components/pages/resetPasswordError/layouts/mobile";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Smart Savings</title>
+        <meta name="description" content="Smart Savings home" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+      <MainLayout
+        desktop={<ResetPasswordErrorDesktop />}
+        mobile={<ResetPasswordErrorMobile />}
+      />
+    </>
+  );
+}
