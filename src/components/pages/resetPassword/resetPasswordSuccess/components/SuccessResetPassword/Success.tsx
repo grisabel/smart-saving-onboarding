abb673e0ba58c5a2ae89180f899062489dafd170
Delgado Trueba <jose.delgado@babel.es>
2024-03-03 18:21:06 +0100

    feat/CA-1.4: resetPassword success

---
+++ src/components/pages/resetPassword/resetPasswordSuccess/components/SuccessResetPassword/Success.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import router from "next/router";
+
+import Button from "@/components/stories/atoms/Buttons/Button";
+import styles from "./Success.module.scss";
+
+const Success: React.FC = () => {
+  const { t } = useTranslation();
+
+  const handleLogin = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className={styles.text}>
+      <div>
+        <p className={styles.text__bold}>{t("congratulation")}</p>
+        <p className={styles.text__thin}>{t("reset-password-completed")}</p>
+      </div>
+      <Image
+        src="/images/retrievePassword/retrivePasswordEmailSentPage.svg"
+        alt="retrievePassword"
+        width={320}
+        height={320}
+      ></Image>
+      <p className={styles.text__bold}>{t("login-now")}</p>
+      <Button label={t("loginButtonLabel")} onClick={handleLogin}></Button>
+    </div>
+  );
+};
+
+export default Success;
---
+++ src/components/pages/resetPassword/resetPasswordSuccess/layouts/mobile/SuccessResetPasswordMobile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import LayoutMobile from "@/components/stories/templates/LayoutMobile";
+
+import styles from "./SuccessResetPasswordMobile.module.scss";
+import DotStep from "@/components/stories/atoms/Steps/DotStep";
+import Success from "../../components/SuccessResetPassword";
+
+function SuccessResetPasswordMobile() {
+  return (
+    <LayoutMobile>
+      <div className={styles.success}>
+        <div className={styles.content}>
+          <Success />
+        </div>
+        <div className={styles.steps}>
+          <DotStep
+            steps={[
+              { isTransited: false },
+              { isTransited: false },
+              { isTransited: false },
+            ]}
+          ></DotStep>
+        </div>
+      </div>
+    </LayoutMobile>
+  );
+}
+
+export default SuccessResetPasswordMobile;
